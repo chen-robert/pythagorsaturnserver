8d63a96e1bbe28b80b2d14b2d58d4ac73a0537d5
Cory Bear <gitlog@corybear.com>
2016-07-19 17:32:25 -0700

    init

---
+++ src/main/java/mazeserver/Main.java
@@ -0,0 +1,170 @@
+/**
+ * Maze Server
+ *
+ * Copyright 2016 Finn Bear.  All rights reserved.
+ */
+
+package mazeserver;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.MessageFormat;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.http.HttpStatus;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
+
+public class Main extends HttpServlet {
+
+    private static final int _MAZE_HEIGHT = 20;
+    private static final int _MAZE_WIDTH = 20;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        assert req != null;
+        assert resp != null;
+        PrintWriter out = resp.getWriter();
+
+        try {
+            resp.setStatus(HttpStatus.OK_200);
+            String p = req.getServletPath();
+            assert p != null;
+            boolean debugging = p.equals("/");
+
+            if (debugging) {
+                maze(new Date().getTime(), out, true);
+            } else {
+                String[] a = p.split("/");
+                String arg = a.length < 2 ? null : a[1];
+                maze(parseMazeId(arg), out, false);
+            }
+
+            resp.setStatus(HttpStatus.OK_200);
+        } catch (Throwable ex) {
+            out.println(ex.toString());
+            resp.setStatus(HttpStatus.OK_200);
+        }
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        assert req != null;
+        assert resp != null;
+        PrintWriter out = resp.getWriter();
+        try {
+            // id = req.getParameter("id");
+            String p = req.getServletPath();
+            assert p != null;
+            String[] a = p.split("/");
+            String arg = a.length < 2 ? null : a[1];
+            assert arg != null;
+            long mazeId = parseMazeId(arg);
+
+            String line;
+            BufferedReader input = req.getReader();
+            while ((line = input.readLine()) != null) {
+                out.print(parseJson(line));
+            }
+
+            resp.setStatus(HttpStatus.OK_200);
+        } catch (Throwable ex) {
+            out.println(ex.toString());
+            resp.setStatus(HttpStatus.OK_200);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+        ServletContextHandler handler = new ServletContextHandler(server, "/maze");
+        handler.addServlet(Main.class, "/");
+        server.start();
+    }
+
+    private void maze(long mazeId, PrintWriter out, boolean debugging) {
+
+        Maze maze = new Maze(mazeId, _MAZE_HEIGHT, _MAZE_WIDTH);
+        Gson gson = new GsonBuilder().create();
+
+        if (debugging) {
+            out.println("<html>\n<title>maze</title>\n<body>");
+        }
+
+        // SVG
+        if (debugging) {
+            StringBuffer svg = new StringBuffer();
+            final int scale = 15;
+            final int unscaledPadding = Math.max(_MAZE_HEIGHT / 2, _MAZE_WIDTH / 2);
+            final int scaledPadding = scale * unscaledPadding;
+            final int scaledHeight = scale * _MAZE_HEIGHT;
+            final int scaledWidth = scale * _MAZE_WIDTH;
+
+            final int xBounds = scaledPadding / 2;
+            final int yBounds = xBounds;
+            final int xCenter = (scaledPadding + scaledWidth) / 2;
+            final int yCenter = (scaledPadding + scaledHeight) / 2;
+            svg.append(MessageFormat.format( "<svg height=\"{0}\" width=\"{1}\">\n",
+                scaledPadding + scaledHeight, scaledPadding + scaledWidth ));
+
+            // Bounding box and center (x, y).
+            svg.append(MessageFormat.format("  <rect x=\"{0}\" y=\"{1}\" height=\"{2}\" width=\"{3}\" style=\"stroke-width:1;stroke:rgb(0,0,255);fill:rgb(255,255,255);stroke-opacity:0.2;\" />\n", xBounds, yBounds, scaledHeight, scaledWidth));
+            svg.append(MessageFormat.format( "  <line x1=\"{0}\" y1=\"{1}\" x2=\"{2}\" y2=\"{3}\" style=\"stroke:rgb(0,0,255);stroke-opacity:0.2;stroke-width:1\" />\n", xCenter, yBounds, xCenter, yBounds + scaledHeight));
+            svg.append(MessageFormat.format( "  <line x1=\"{0}\" y1=\"{1}\" x2=\"{2}\" y2=\"{3}\" style=\"stroke:rgb(0,0,255);stroke-opacity:0.2;stroke-width:1\" />\n", xBounds, yCenter, xBounds + scaledWidth, yCenter));
+
+            for (Maze.Line line : maze.getContent().getLines()) {
+                svg.append(MessageFormat.format( "  <line x1=\"{0}\" y1=\"{1}\" x2=\"{2}\" y2=\"{3}\" style=\"stroke:rgb(255,0,0);stroke-width:2\" />\n",
+                    xCenter + scale * line.getStart().getX(),
+                    yCenter + scale * line.getStart().getY(),
+                    xCenter + scale * line.getEnd().getX(),
+                    yCenter + scale * line.getEnd().getY() ));
+            }
+            svg.append("</svg>\n");
+            out.println(svg.toString());
+        }
+
+        // JSON
+        if (debugging) {
+            out.println("<h1>json</h1>\n<a href=\"" + mazeId + "\">JSON only</a>\n<pre>\n");
+        }
+        gson.toJson(maze.getContent(), out);
+        if (debugging) {
+            out.println("\n</pre>");
+            out.println("<form action=\"" + mazeId + "\" enctype=\"multipart/form-data\" method=\"POST\" name=\"play\">");
+            out.println("<input name=\"json\" type=\"file\" />");
+            out.println("<input type=\"submit\" value=\"Play\" />");
+            out.println("</form>");
+            out.println("</body>\n</html>");
+        }
+    }
+
+    private static String parseJson(String arg) {
+        String json = null;
+        if (arg != null) {
+            int i = arg.indexOf("{");
+            int j = arg.lastIndexOf("}");
+            if (i >= 0 && j >= 0 && i < j) {
+                json = arg.substring(i, j + 1);
+            } else {
+                json = "";
+            }
+        }
+        return json;
+    }
+
+    private static long parseMazeId(String arg) {
+        long mazeId;
+        try {
+            mazeId = Long.parseLong(arg);
+        } catch (NumberFormatException ex) {
+            mazeId = 13579111315L;
+        }
+        return mazeId;
+    }
+}
---
+++ src/main/java/mazeserver/Maze.java
@@ -0,0 +1,265 @@
+/**
+ * Maze Server
+ *
+ * Copyright 2016 Finn Bear.  All rights reserved.
+ */
+package mazeserver;
+
+import com.google.gson.annotations.SerializedName;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+
+public class Maze {
+
+    private Content _content;
+    private int _height;
+    private Map<Long, Line> _lines;
+    private Random _random;
+    private int _width;
+
+
+    public Maze(long mazeId, int height, int width) {
+        assert height >= 10;
+        assert width >= 10;
+
+        _height = height;
+        _lines = new HashMap();
+        _random = new Random(mazeId);
+        _width = width;
+    }
+
+    public Line addHorizontal(int x, int y, int length) {
+        return addLine(x, y, 0, length);
+    }
+
+    /**
+     * Add a line at the specified coordinates.
+     * Merge with existing lines if possible.
+     */
+    public Line addLine(int x, int y, int angle, int length) {
+        assert angle >= 0;
+        assert length >= 0;
+        assert y >= 0;
+        assert y >= 0;
+
+        Line line = getLine(x, y, angle);
+        if (line == null) {
+            line = new Line(this, x, y, angle, length);
+            putLine(line.getEndTuple(), line);
+        } else {
+            line.extend(length);
+        }
+        return line;
+    }
+
+    public Line addVertical(int x, int y, int length) {
+        return addLine(x, y, 90, length);
+    }
+
+    public Content getContent() {
+        if (_content == null) {
+            generate();
+            assert _lines != null;
+            _content = new Content(_lines.values());
+        }
+
+        return _content;
+    }
+
+    public void generate() {
+        generate(-_width / 2, -_height / 2, _width, _height);
+    }
+
+    public void generate(int x, int y, int w, int h) {
+        assert h >= 0;
+        assert w >= 0;
+
+        _content = null;
+        if (w >= 2 && h >= 2) {
+            // Bisect horizontally and vertically at (px, py).
+            int rx = w == 2 ? 1 : 1 + _random.nextInt(w - 1);
+            int ry = h == 2 ? 1 : 1 + _random.nextInt(h - 1);
+            assert rx > 0 && rx < w && ry > 0 && ry < h;
+            int px = x + rx;
+            int py = y + ry;
+            Line vertical = addVertical(px, y, h);
+            Line horizontal = addHorizontal(x, py, w);
+
+            // Flip a coin as to whether to cut the horizontal twice or the vertical.
+            boolean cutHorizontal = _random.nextInt(2) == 0;
+            if (cutHorizontal) {
+                // Make the rightmost cut first so the pointer stays valid.
+                horizontal.cut(rx + _random.nextInt(w - rx));
+                horizontal.cut(_random.nextInt(rx));
+                vertical.cut(_random.nextInt(ry));
+            } else {
+                // Make the bottomost cut first so the pointer stays valid.
+                vertical.cut(ry + _random.nextInt(h - ry));
+                vertical.cut(_random.nextInt(ry));
+                horizontal.cut(_random.nextInt(rx));
+            }
+
+            // Recursively build the sub-mazes.
+            generate(px, y, w - rx, ry); // Quadrant I if (0, 0) is top left.
+            generate(x, y, rx, ry); // Quadrant II " ".
+            generate(x, py, rx, h - ry); // Quadrant III " ".
+            generate(px, y, w - rx, h - ry); // Quadrant IV " ".
+        }
+    }
+
+    public Line getLine(int x, int y, int angle) {
+        assert _lines != null;
+        return _lines.get(tuple(x, y, angle));
+    }
+
+    public void putLine(Long key, Line line) {
+        assert _lines != null;
+
+        _lines.put(key, line);
+    }
+
+    public void removeLine(Long key) {
+        assert _lines != null;
+
+        Line line = _lines.remove(key);
+        assert line != null;
+    }
+
+    private static Long tuple(int x, int y, int angle) {
+        return new Long(((angle & 0xFFF) << 48) | ((y & 0xFFFFF) << 24) | (x & 0xFFFFF));
+    }
+
+    public static class Content {
+        @SerializedName("Lines")
+        Collection<Line> _lines;
+
+        public Content(Collection<Line> lines) {
+            assert lines != null;
+            _lines = new ArrayList<Line>(lines);
+            // Populate "end" point.
+            for (Line i : _lines) {
+                i.getEnd();
+            }
+        }
+
+        public Collection<Line> getLines() {
+            return _lines;
+        }
+    }
+
+    public static class Line {
+
+        private transient int _angle;
+        @SerializedName("A")
+        private Point _start; // Must precede "b" here so that it does in the JSON.
+        @SerializedName("B")
+        private Point _endCache;
+        private transient int _length;
+        private transient Maze _maze;
+
+
+        public Line(Maze maze, int x, int y, int angle, int length) {
+            _angle = angle;
+            _length = length;
+            _maze = maze;
+            _start = new Point(x, y);
+        }
+
+        public void cut(int length) {
+            assert length >= 0;
+            assert _maze != null;
+
+            int oldLength = _length;
+            int diff = _length - length;
+            if (diff == 1) {
+                _maze.removeLine(getEndTuple());
+                _length = length;
+                _endCache = null;
+                _maze.putLine(getEndTuple(), this);
+            } else if (diff > 1) {
+                _maze.removeLine(getEndTuple());
+                _length = length + 1;
+                _endCache = null;
+                Point start = getEnd();
+                _length = length;
+                _endCache = null;
+
+                _maze.putLine(getEndTuple(), this);
+                _maze.addLine(start.getX(), start.getY(), _angle, oldLength - _length - 1);
+            }
+        }
+
+        public void extend(int length) {
+            assert length >= 0;
+            assert _maze != null;
+
+            _maze.removeLine(getEndTuple());
+            _length += length;
+            _endCache = null;
+            _maze.putLine(getEndTuple(), this);
+        }
+
+        public Point getEnd() {
+            if (_endCache == null) {
+                Point start = getStart();
+                int x;
+                int y;
+                switch (_angle) {
+                    case 0:
+                        x = start.getX() + _length;
+                        y = start.getY();
+                        break;
+
+                    case 90:
+                        x = start.getX();
+                        y = start.getY() + _length;
+                        break;
+
+                    default:
+                        x = y = -1;
+                        assert false;
+                }
+                _endCache = new Point(x, y);
+            }
+
+            return _endCache;
+        }
+
+        public Long getEndTuple() {
+            Point end = getEnd();
+            return Maze.tuple(end.getX(), end.getY(), _angle);
+        }
+
+        public Point getStart() {
+            assert _start != null;
+            return _start;
+        }
+    }
+
+    public static class Point {
+
+        @SerializedName("x")
+        private int _x;
+
+        @SerializedName("y")
+        private int _y;
+
+
+        public Point(int x, int y) {
+            _x = x;
+            _y = y;
+        }
+
+        public int getX() {
+            return _x;
+        }
+
+        public int getY() {
+            return _y;
+        }
+    }
+}
