f62533754aada5531868ad945acfdbd616edf7b8
Cory Bear <gitlog@corybear.com>
2016-07-21 18:50:19 -0700

    Start in correct directory

--- restart.sh
+++ restart.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
-cd $WORKSPACE
+if [ "$WORKSPACE" == "" ]
+then
+    echo WORKSPACE is missing
+    pwd
+else
+    echo WORKSPACE is "$WORKSPACE"
+    cd $WORKSPACE
+    pwd
+fi
 J="target/mazeserver-1.0-jar-with-dependencies.jar"
-echo pkill -f $J
+ls -l "$J"
+echo pkill -f "$J"
 pkill -f $J
-echo nohup java -jar $J
-nohup java -jar $J &
+echo nohup java -jar "$J"
+nohup java -jar "$J" >/tmp/mazeserver.out 2>&1 &
+sleep 0.2
+pgrep -f "$J"
